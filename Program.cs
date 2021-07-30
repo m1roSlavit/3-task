faf32b2f5ea305484516ffb585484b6e53d68e27
m1r0slav <m1roslavsavit@gmail.com>
2021-07-30 23:52:55 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using Org.BouncyCastle.Security;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] inputMoves = Console.ReadLine().Split(' ');
+            int movesCount = inputMoves.Length;
+            int compChoiceIdx = rnd.Next(0, movesCount);    
+            if(movesCount % 2 == 0 || movesCount < 3)
+            {
+                Console.WriteLine("Not valid data");
+                return;
+            }
+
+            byte[] HMACkeyBytes = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(HMACkeyBytes);
+            string HMACKey = String.Join("", BitConverter.ToString(HMACkeyBytes).Split('-'));
+
+            var hash = new HMACSHA256(HMACkeyBytes);
+            byte[] compChoiceBytes = Encoding.ASCII.GetBytes(inputMoves[compChoiceIdx]);
+            byte[] HMACValue = hash.ComputeHash(compChoiceBytes);
+            Console.WriteLine("HMAC:");
+            Console.WriteLine(String.Join("", BitConverter.ToString(HMACValue).Split('-')));
+
+
+            Console.WriteLine("Available moves:");
+            for (int i = 0; i < inputMoves.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {inputMoves[i]}");
+            }
+            Console.WriteLine($"0. exit");
+            Console.WriteLine("Enter your move:");
+            int userChoiceIdx;
+            while (true)
+            {
+                int userChoiceIdxTemp = Convert.ToInt32(Console.ReadLine());
+                if (userChoiceIdxTemp == 0)
+                {
+                    return;
+                }
+                if (userChoiceIdxTemp > 0 && userChoiceIdxTemp <= movesCount)
+                {
+                    userChoiceIdx = userChoiceIdxTemp - 1;
+                    break;
+                }
+            }
+            Console.WriteLine($"Your move: {inputMoves[userChoiceIdx]}");
+            Console.WriteLine($"Computer move: {inputMoves[compChoiceIdx]}");
+
+            userChoiceIdx++;
+            compChoiceIdx++;
+
+            if (userChoiceIdx < (movesCount / 2) + 1)
+            {
+                if (compChoiceIdx - userChoiceIdx < movesCount / 2)
+                {
+                    Console.WriteLine("Comp Win");
+                }else{
+                    Console.WriteLine("You Win");
+                }
+            } else if(userChoiceIdx > (movesCount / 2) + 1) {
+                if (userChoiceIdx - compChoiceIdx < movesCount / 2)
+                {
+                    Console.WriteLine("You Win");
+                }
+                else
+                {
+                    Console.WriteLine("Comp Win");
+                }
+            } else
+            {
+                if (compChoiceIdx < userChoiceIdx)
+                {
+                    Console.WriteLine("You Win");
+                }
+                else if (compChoiceIdx > userChoiceIdx)
+                {
+                    Console.WriteLine("Comp Win");
+                } else
+                {
+                    Console.WriteLine("Draw");
+                }
+            }
+
+            Console.WriteLine($"HMAC key: {HMACKey}");
+        }
+    }
+}
